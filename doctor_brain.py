dbd7650eb22b2596b91b8264f5b3d51acd75351d
kachastepien <trafficjam027@gmail.com>
2025-06-14 20:12:31 +0200

    dodane duzo nowych rzeczy, poprawki w doctor_brain.py, doctor_voice.py, patient_voice.py

--- doctor_brain.py
+++ doctor_brain.py
@@ -9,41 +9,42 @@ load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 if not GROQ_API_KEY:
     raise ValueError("GROQ_API_KEY is not set in the environment variables.")
-# OPENAI_API_KEY = "..." # Not needed unless you're using OpenAI too
 
 # Encode image
-image_path = "acne.jpg"
-with open(image_path, "rb") as image_file:
-    encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
+#image_path = "acne.jpg"
 
-# Setup Groq client
-client = Groq(api_key=GROQ_API_KEY)
+def encode_image(image_path):   
+    image_file=open(image_path, "rb")
+    return base64.b64encode(image_file.read()).decode('utf-8')
 
-query = "co ja mam kurwa na twarzy? czy to trądzik? co mam z tym zrobić?"
+#Step3: Setup Multimodal LLM 
+from groq import Groq
+
+query="Is there something wrong with my face?"
 model = "meta-llama/llama-4-scout-17b-16e-instruct"
+#model="llama-3.2-90b-vision-preview" #Deprecated
 
-messages = [
-    {
-        "role": "system",
-        "content": "Ej, ziomuś – masz odpowiadać jak kumpel z osiedla: na luzie, swojsko, bez zadęcia. Jak trzeba, to rzucaj luźnym tekstem, ale dalej konkretnie i pomocnie, okej?"
-    },
-    {
-        "role": "user",
-        "content": [
-            {"type": "text", "text": query},
-            {
-                "type": "image_url",
-                "image_url": {
-                    "url": f"data:image/jpeg;base64,{encoded_image}",
+def analyze_image_with_query(query, model, encoded_image):
+    client=Groq()  
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text", 
+                    "text": query
                 },
-            },
-        ],
-    }
-]
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{encoded_image}",
+                    },
+                },
+            ],
+        }]
+    chat_completion=client.chat.completions.create(
+        messages=messages,
+        model=model
+    )
 
-chat_completion = client.chat.completions.create(
-    model=model,
-    messages=messages
-)
-
-print(chat_completion.choices[0].message.content)
+    return chat_completion.choices[0].message.content
